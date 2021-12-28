61f804a78262e792f25c8bbd69da94d38f2270aa
Oleg Golenischev <oleg-golenischev63@yandex.ru>
2021-12-28 16:03:04 +0300

    Sum of numbers added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int first = int.Parse(Console.ReadLine() ?? "0");
+int second = int.Parse(Console.ReadLine() ?? "0");
+int sum = first+ second;
+Console.WriteLine($"Sum of numbers: {sum}");
